e7cc564abf1e7d852e92836f6490dac322e7614c
Mofi <105859019+metahouses@users.noreply.github.com>
2024-03-19 21:16:08 +0800

    Update conf.py
    更改部分信息到个人端

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "墨鸢博客发布指南"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+author = "墨非未已"
+email = "保密"
+author_homepage = "暂无"
+description = "青春未已，翰墨非常。"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
